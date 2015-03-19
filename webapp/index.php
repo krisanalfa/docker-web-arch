8abf9b45acacdaeb639bb6a9126b4d8088688646
Krisan Alfa Timur <krisan47@gmail.com>
2015-03-19 22:00:37 +0700

    [FIX] Linking over containers.

---
+++ webapp/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Docker Web Arch</title>
+        <style>
+            body { margin: 0; padding: 0; width: 100%; height: 100%; color: #7E7E7E; display: table; font-family: 'Lato'; }
+            .container { text-align: center; display: table-cell; vertical-align: middle; }
+            .content { text-align: center; display: inline-block; }
+            .title { font-size: 96px; margin-bottom: 40px; }
+            .quote { font-size: 24px; }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Docker Web Arch</div>
+                <div class="quote">To view your phpinfo, you may go <a href="/info.php">here</a>.</div>
+            </div>
+        </div>
+    </body>
+</html>
